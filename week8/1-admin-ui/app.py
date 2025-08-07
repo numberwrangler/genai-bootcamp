26ee734efa506267f7b59f4a3ea846fb488014bf
Blake Lawall <44737710+numberwrangler@users.noreply.github.com>
2025-08-07 10:07:13 -0600

    Update app.py

--- week8/1-admin-ui/app.py
+++ week8/1-admin-ui/app.py
@@ -15,7 +15,7 @@ _ = Twin(app, "Twin",
          kb_data_src_id=kb.kb.data_source_id,
          kb_input_bucket=kb.input_bucket,
          env=env,
-         custom_domain_name="numberwrangler.net",  # Replace with your domain
+         custom_domain_name="twin.numberwrangler.net",  # Replace with your domain
          custom_certificate_arn="arn:aws:acm:us-east-1:845422352941:certificate/cc28962c-4c16-46ee-ae43-f36c5c8b6a51"  # Replace with your certificate ARN
 )
 
